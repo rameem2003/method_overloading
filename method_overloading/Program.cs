cbab3d77112bfc213e0eab2311adec2f71608541
rameem2003 <rameem2019@gmail.com>
2022-03-01 22:07:25 +0600

    Add project files.

---
+++ method_overloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace method_overloading
+{
+    class app
+    {
+        public static void Main()
+        {
+            int a = 50;
+            int b = 50;
+            int c = 40;
+
+            app obj = new app();
+
+            obj.function(a, b, c);
+            obj.function(a, b);
+        }
+
+
+        public void function(int a, int b, int c)
+        {
+            int result = a+ b + c;
+            Console.WriteLine("Sum of 3 number's is: " + result);
+        }
+
+        public void function(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Sum of 2 number's is: " + result);
+        }
+    }
+}
